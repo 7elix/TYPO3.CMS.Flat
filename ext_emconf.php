59f1188a444e8f953a1d002449e852983bdbe213
Felix Kopp <felix-source@phorax.com>
2013-08-12 00:55:46 +0200

    Initial

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+/***************************************************************
+ * Extension Manager/Repository config file for ext "flat".
+ *
+ * Auto generated 25-10-2011 13:11
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Flat skin',
+	'description' => '',
+	'category' => 'be',
+	'shy' => 0,
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => 0,
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author' => 'Felix Kopp',
+	'author_email' => 'felix-source@phorax.com',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'version' => '6.2.0',
+	'_md5_values_when_last_written' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.0-6.2.99',
+		),
+		'conflicts' => array(
+			't3skin'
+		),
+		'suggests' => array()
+	),
+	'suggests' => array()
+);
+?>
