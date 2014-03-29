b592eb20637d9b236baf3071ee7ac6e1dae0f0b3
Felix Kopp <felix-source@phorax.com>
2014-03-29 15:35:39 +0100

    [TASK] ext_emconf cleanup

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,4 +1,5 @@
 <?php
+
 /***************************************************************
  * Extension Manager/Repository config file for ext "flat".
  *
@@ -8,11 +9,13 @@
  * Only the data in the array - everything else is removed by next
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
+
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Flat skin',
 	'description' => 'First backend theme with strong focus on your content. Buggy but usable. Stable but alpha. Dark but light.',
 	'category' => 'be',
 	'shy' => 0,
+	'version' => '7.0.0',
 	'dependencies' => 't3skin',
 	'conflicts' => '',
 	'priority' => '',
@@ -28,18 +31,24 @@ $EM_CONF[$_EXTKEY] = array(
 	'author' => 'Felix Kopp',
 	'author_email' => 'felix-source@phorax.com',
 	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'version' => '7.0.0',
-	'_md5_values_when_last_written' => '',
-	'constraints' => array(
-		'depends' => array(
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
 			'typo3' => '6.2.0-6.2.99',
 			't3skin' => '6.2.0-6.2.99'
 		),
-		'conflicts' => array(
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
 		),
 		'suggests' => array()
 	),
 	'suggests' => array()
 );
+
+?>
