3b3e4fee345cdfe47b5528448a061e9106cf408e
Felix Kopp <felix-source@phorax.com>
2014-09-02 00:28:45 +0200

    [FEATURE] Change action icons to FontAwesome buttons
    Depends on IconUtility core patch.
    
    See: https://review.typo3.org/#/c/32542/
    See: https://forge.typo3.org/issues/61289

--- Classes/Hooks/IconUtility.php
+++ Classes/Hooks/IconUtility.php
@@ -30,23 +30,40 @@ namespace PHORAX\Flat\Hooks;
 class IconUtility {
 
 	static public $flatSpriteIconName = array(
-		'actions-document-new' => 'fa-plus-square',
-		'actions-document-open' => 'fa-pencil',
-		'actions-document-info' => 'fa-info-circle',
-		'actions-document-view' => 'fa-play-circle',
-		'actions-document-history-open' => 'fa-history',
-		'actions-document-move' => 'fa-arrows',
-		'actions-document-save' => 'fa-save',
+		't3-icon t3-icon-actions t3-icon-actions-document t3-icon-document-new' => 'fa-plus-square',
+		't3-icon t3-icon-actions t3-icon-actions-document t3-icon-document-open' => 'fa-pencil',
+		't3-icon t3-icon-actions t3-icon-actions-document t3-icon-document-close' => 'fa-close',
+		't3-icon t3-icon-actions t3-icon-actions-document t3-icon-document-info' => 'fa-info-circle',
+		't3-icon t3-icon-actions t3-icon-actions-document t3-icon-document-view' => 'fa-play-circle',
+		't3-icon t3-icon-actions t3-icon-actions-document t3-icon-document-history-open' => 'fa-history',
+		't3-icon t3-icon-actions t3-icon-actions-document t3-icon-document-move' => 'fa-arrows',
+		't3-icon t3-icon-actions t3-icon-actions-document t3-icon-document-save' => 'fa-save',
 
-		'actions-system-refresh' => 'fa-refresh',
-		'actions-system-help-open' => 'fa-question-circle',
+		't3-icon t3-icon-actions t3-icon-actions-window t3-icon-window-open' => 'fa-arrows-alt',
+
+		't3-icon t3-icon-actions t3-icon-actions-system t3-icon-system-refresh' => 'fa-refresh',
+		't3-icon t3-icon-actions t3-icon-actions-system t3-icon-system-help-open' => 'fa-question-circle',
 		't3-icon t3-icon-actions t3-icon-actions-system t3-icon-system-shortcut-new' => 'fa-star',
-		'actions-system-tree-search-open' => 'fa-search',
-		'actions-system-extension-download' => 'fa-download',
-		'actions-system-cache-clear' => 'fa-bolt',
+		't3-icon t3-icon-actions t3-icon-actions-system t3-icon-system-tree-search-open' => 'fa-search',
+		't3-icon t3-icon-actions t3-icon-actions-system t3-icon-system-cache-clear' => 'fa-bolt',
+
+		't3-icon t3-icon-actions t3-icon-actions-system t3-icon-system-extension-download' => 'fa-download',
+		't3-icon t3-icon-actions t3-icon-system-extension-import' => 'fa-cloud-download',
+		't3-icon t3-icon-actions t3-icon-actions-system t3-icon-system-extension-sqldump' => 'fa-database',
+		't3-icon t3-icon-actions t3-icon-actions-system t3-icon-system-extension-install' => 'fa-plus-circle',
+		't3-icon t3-icon-actions t3-icon-actions-system t3-icon-system-extension-uninstall' => 'fa-minus-square',
+		't3-icon t3-icon-actions t3-icon-actions-system t3-icon-system-extension-download ' => 'fa-cloud-download',
+		't3-icon t3-icon-actions t3-icon-actions-system t3-icon-system-extension-configure' => 'fa-gear',
+		't3-icon t3-icon-extensions t3-icon-extensions-extensionmanager t3-icon-extensionmanager-update-script' => 'fa-refresh',
+
+		't3-icon t3-icon-actions t3-icon-system-extension-import' => 'fa-cloud-download',
+
+		't3-icon t3-icon-mimetypes t3-icon-mimetypes-word t3-icon-word' => 'fa-file-world-o',
+		't3-icon t3-icon-mimetypes t3-icon-mimetypes-pdf t3-icon-pdf' => 'fa-file-pdf-o',
+		't3-icon t3-icon-mimetypes t3-icon-mimetypes-text t3-icon-text-html' => 'fa-file-html-o',
 
 		't3-icon t3-icon-actions t3-icon-actions-edit t3-icon-edit-pick-date' => 'fa-calendar',
-		't3-icon t3-icon-actions t3-icon-actions-edit t3-icon-edit-delete' => 'fa-remove',
+		't3-icon t3-icon-actions t3-icon-actions-edit t3-icon-edit-delete' => 'fa-trash',
 		't3-icon t3-icon-actions t3-icon-actions-edit t3-icon-edit-hide' => 'fa-circle',
 		't3-icon t3-icon-actions t3-icon-actions-edit t3-icon-edit-unhide' => 'fa-circle-thin',
 		't3-icon t3-icon-actions t3-icon-actions-edit t3-icon-edit-cut' => 'fa-cut',
@@ -54,36 +71,51 @@ class IconUtility {
 		't3-icon t3-icon-actions t3-icon-actions-edit t3-icon-edit-paste' => 'fa-paste',
 		't3-icon t3-icon-actions t3-icon-actions-edit t3-icon-edit-upload' => 'fa-upload',
 
-		'actions-page-move' => 'fa-arrows',
-		'actions-page-open' => 'fa-pencil',
-		'actions-page-new' => 'fa-plus-square',
+		't3-icon t3-icon-actions t3-icon-actions-page t3-icon-page-move' => 'fa-arrows',
+		't3-icon t3-icon-actions t3-icon-actions-page t3-icon-page-open' => 'fa-pencil',
+		't3-icon t3-icon-actions t3-icon-actions-page t3-icon-page-new' => 'fa-plus-square',
 
-		'status-status-locked' => 'fa-lock',
+		't3-icon t3-icon-status t3-icon-status-status t3-icon-status-locked' => 'fa-lock',
+		't3-icon t3-icon-status t3-icon-status-status t3-icon-status-readonly' => 'fa-lock',
 
-		'actions-move-up' => 'fa-arrow-up',
-		'actions-move-down' => 'fa-arrow-down',
-		'actions-move-left' => 'fa-arrow-left',
-		'actions-move-right' => 'fa-arrow-right',
+		't3-icon t3-icon-actions t3-icon-actions-view t3-icon-view-table-expand' => 'fa-angle-double-right',
 
-		'actions-view-go-up' => 'fa-level-up',
-		'actions-view-go-back' => 'fa-angle-double-left'
+		't3-icon t3-icon-actions t3-icon-actions-move t3-icon-move-up' => 'fa-arrow-up',
+		't3-icon t3-icon-actions t3-icon-actions-move t3-icon-move-down' => 'fa-arrow-down',
+		't3-icon t3-icon-actions t3-icon-actions-move t3-icon-move-left' => 'fa-arrow-left',
+		't3-icon t3-icon-actions t3-icon-actions-move t3-icon-move-right' => 'fa-arrow-right',
+
+		't3-icon t3-icon-status t3-icon-status-status t3-icon-status-permission-granted' => 'fa-check-circle-o',
+		't3-icon t3-icon-status t3-icon-status-status t3-icon-status-permission-denied' => 'fa-minus-square',
+		't3-icon t3-icon-status t3-icon-status-warning t3-icon-warning-lock' => 'fa-lock',
+
+		't3-icon t3-icon-actions t3-icon-actions-view t3-icon-view-go-up' => 'fa-level-up',
+		't3-icon t3-icon-actions t3-icon-actions-view t3-icon-view-go-back' => 'fa-angle-double-left',
+		't3-icon t3-icon-actions t3-icon-actions-view t3-icon-view-go-forward' => 'fa-angle-double-right',
+
+		't3-icon t3-icon-actions t3-icon-actions-view t3-icon-view-paging-first' => 'fa-step-backward',
+		't3-icon t3-icon-actions t3-icon-actions-view t3-icon-view-paging-previous' => 'fa-arrow-left',
+		't3-icon t3-icon-actions t3-icon-actions-view t3-icon-view-paging-next' => 'fa-arrow-right',
+		't3-icon t3-icon-actions t3-icon-actions-view t3-icon-view-paging-last' => 'fa-step-forward',
+
+#		't3-icon t3-icon-actions t3-icon-actions-system t3-icon-system-backend-user-switch' => '',
+
+		't3-icon t3-icon-status t3-icon-status-dialog t3-icon-dialog-error' => 'fa-exclamation-triangle'
 	);
 
+	/**
+	 * Hook to manipulate IconUtility html output code
+	 *
+	 * @param array $tagAttributes
+	 * @param null $innerHtml
+	 * @param null $tagName
+	 */
 	public function buildSpriteHtmlIconTag(array &$tagAttributes, &$innerHtml = NULL, &$tagName = NULL) {
-
 		$class = self::$flatSpriteIconName[$tagAttributes['class']];
-
 		if ($class) {
-			unset($tagAttributes);
-
-			$tagAttributes[] = array(
-				'attribute' => 'class',
-				'value' => 't3-icon-fa fa fa-lg ' . $class
-			);
-
-			$innerHtml = '';
-			$tagName = 'i';
-
+			$tagAttributes['class'] = 't3-icon-fa fa fa-lg fa-fw ' . $class;
+			// Keep tag name - since JavaScript selectors might depend on <span>
+#			$tagName = 'i';
 		}
 	}
 
